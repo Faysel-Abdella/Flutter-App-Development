d5c74a2935d87f33a0a607b707a9386d70857bad
faysel abdella <fayselcode@gmail.com>
2023-12-24 15:26:11 +0300

    Create the correction logic

--- lib/screens/login_process/cell_certified.dart
+++ lib/screens/login_process/cell_certified.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_app_gap/components/button.dart';
 import 'package:get/get.dart';
 
 class CellCertifiedPage extends StatefulWidget {
@@ -12,7 +11,8 @@ class CellCertifiedPage extends StatefulWidget {
 
 class _CellCertifiedPageState extends State<CellCertifiedPage> {
   late TextEditingController _phoneController;
-  RxBool _isNumberIncorrect = false.obs;
+  late bool _isNumberIncorrect;
+  late bool _isButtonNotYellow;
 
   final _focusNode = FocusNode();
 
@@ -20,6 +20,8 @@ class _CellCertifiedPageState extends State<CellCertifiedPage> {
   void initState() {
     super.initState();
     _phoneController = TextEditingController();
+    _isNumberIncorrect = false;
+    _isButtonNotYellow = true;
 
     // Set the focus on the text field when the screen is shown
     WidgetsBinding.instance
@@ -37,22 +39,28 @@ class _CellCertifiedPageState extends State<CellCertifiedPage> {
     String phoneNumber = _phoneController.text;
     // Hi Kim's developer for the below use the actual real data
     // 아래의 김안녕님 개발자는 실제 실제 데이터를 사용합니다.
-    if (phoneNumber == "0911223344") {
+    if (phoneNumber == "0000000000") {
       // It is the condition where the inserted phone number is not correct or already used
       // 입력한 전화번호가 정확하지 않거나 이미 사용된 전화번호인 경우입니다.
       setState(() {
-        _isNumberIncorrect.value = true;
+        _isNumberIncorrect = true;
+        _isButtonNotYellow = true;
       });
     } else {
-      setState(() {
-        _isNumberIncorrect.value = false;
-      });
       if (phoneNumber.length == 10) {
         setState(() {
-          _isNumberIncorrect.value = false;
+          _isNumberIncorrect = false;
+          _isButtonNotYellow = false;
         });
         // Perform actions for correct input
       }
+    }
+
+    if (phoneNumber == "" || phoneNumber.length < 10) {
+      setState(() {
+        _isButtonNotYellow = true;
+        _isNumberIncorrect = false;
+      });
     }
   }
 
@@ -127,14 +135,15 @@ class _CellCertifiedPageState extends State<CellCertifiedPage> {
                     // Hi kim, use the maximum length of the country phone number
                     maxLength: 10,
                     decoration: InputDecoration(
-                        hintText: 'Enter your phone',
+                        hintText: '번호 입력 (숫자만)',
                         hintStyle: const TextStyle(color: Color(0xFFAEAEB2)),
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12),
                         ),
-                        focusedBorder: _isNumberIncorrect.value
+                        focusedBorder: _isNumberIncorrect
                             ? OutlineInputBorder(
-                                borderSide: const BorderSide(color: Colors.red),
+                                borderSide:
+                                    const BorderSide(color: Color(0xFFFF0089)),
                                 borderRadius: BorderRadius.circular(12),
                               )
                             : (_phoneController.text.length == 10
@@ -148,11 +157,18 @@ class _CellCertifiedPageState extends State<CellCertifiedPage> {
                                     borderSide: const BorderSide(
                                         color: Colors.transparent),
                                   )),
-                        suffixIcon: _isNumberIncorrect.value
-                            ? const Icon(Icons.warning, color: Colors.red)
+                        suffixIcon: _isNumberIncorrect
+                            ? Image.asset(
+                                "images/warning.png",
+                                height: 16,
+                                width: 16,
+                              )
                             : (_phoneController.text.length == 10
-                                ? const Icon(Icons.check_circle,
-                                    color: Colors.yellow)
+                                ? Image.asset(
+                                    "images/correct.png",
+                                    height: 16,
+                                    width: 16,
+                                  )
                                 : null),
                         filled: true,
                         fillColor: _phoneController.text.isNotEmpty
@@ -166,18 +182,49 @@ class _CellCertifiedPageState extends State<CellCertifiedPage> {
                       _checkPhoneNumber();
                     },
                   ),
-                  if (_isNumberIncorrect.value)
+                  if (_isNumberIncorrect)
                     const Align(
                       alignment: Alignment.centerLeft,
                       child: Text(
-                        'Number is incorrect',
+                        '이미 가입된 휴대폰 번호 입니다.',
                         style: TextStyle(
-                          color: Colors.red,
+                          color: Color(0xFFFF0089),
                           fontSize: 10,
                           fontWeight: FontWeight.w700,
                         ),
                       ),
                     ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 118),
+                    height: 36,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: _isButtonNotYellow
+                            ? MaterialStateProperty.all<Color>(
+                                const Color(0xFFAEAEB2))
+                            : MaterialStateProperty.all<Color>(
+                                const Color(0xFFDBFF00)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        // Handle button press
+                      },
+                      child: const Text(
+                        "인증요청",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF242426),
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
             ],
